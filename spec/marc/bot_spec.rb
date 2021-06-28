22249c9a8d9d6cd4027901e80346b63e13c7f944
Adam Wead <awead@users.noreply.github.com>
2021-06-28 10:19:09 -0400

    Adding x86_64-linux platform for CI

--- spec/marc/bot_spec.rb
+++ spec/marc/bot_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Marc::Bot do
   it "has a version number" do
     expect(Marc::Bot::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
